ac328c4a8097beb500059078c304ca76615e5830
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-09-05 08:38:12 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- NoxxNetwork/modules/stats.py
+++ NoxxNetwork/modules/stats.py
@@ -1,5 +1,5 @@
 from pyrogram import filters, Client
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram.types import Message
 
 from NoxxNetwork import OWNER, NoxxBot
 from NoxxNetwork.database.chats import get_served_chats
@@ -7,25 +7,12 @@ from NoxxNetwork.database.users import get_served_users
 
 
 @NoxxBot.on_message(filters.command("stats") & filters.user(OWNER))
-async def stats_cmd(cli: Client, message: Message):
-    keyboard = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("📊 Show Stats", callback_data="show_stats")]]
-    )
-    await message.reply_text(
-        "ʜᴇʏ ᴍᴀsᴛᴇʀ ✨\nᴛᴀᴘ ᴛʜᴇ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ᴠɪᴇᴡ ʟɪᴠᴇ sᴛᴀᴛs 📊",
-        reply_markup=keyboard,
-    )
-
-
-@NoxxBot.on_callback_query(filters.regex("show_stats"))
-async def show_stats(cli: Client, query: CallbackQuery):
+async def stats(cli: Client, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
-    bot = await cli.get_me()
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
 
-    text = f"""📊 ʙᴏᴛ sᴛᴀᴛs ᴏғ {NoxxBot.username}
-
-➻ ᴄʜᴀᴛs : {chats}
-➻ ᴜsᴇʀs : {users}"""
-
-    await query.answer(text, show_alert=True)
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
